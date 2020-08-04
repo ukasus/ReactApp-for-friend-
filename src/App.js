ce754bb1d759b7936ca5f60b9dd3f4ced81fe147
Lakshay1729 <garglakshay56@gmail.com>
2020-08-04 13:42:12 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,123 @@ import Cakes from './components/Cakes.js';
 import OrderConfirmation from './components/OrderConfirmation.js';
 import OrderCatalog from './components/OrderCatalog';
 
+import Particles from 'react-particles-js';
+
+
+
+
+
 class App extends Component{
 
   render()
   {
     return(
+
+      
+      <div >
+         <div id="particles">
+         <Particles 
+               params={{ 
+                particles: { 
+                    anim:{
+                    enable:true,
+                        speed:6,
+
+                    },
+                    line_linked:{
+                        width:.2
+                    },
+                  number: { 
+                    value: 100, 
+                    density: { 
+                      enable: true, 
+                      value_area: 1300, 
+                    } 
+                  },
+                  shape: {
+                    type: "circle",
+                    stroke: {
+                      width: 1,
+                      color: "#ffffff"
+                    },
+              
+                }, 
+                move: {
+                    enable: true,
+                    speed: 4,
+                    direction: "none",
+                    random: true,
+                    straight: true,
+                    out_mode: "out",
+                    
+                  },
+                  interactivity: {
+                    detect_on: "canvas",
+                    events: {
+                      onhover: {
+                        enable:true,
+                        mode: "attract"
+                      },
+                      onclick: {
+                        enable: true,
+                        mode: "push"
+                      },
+                      resize: true
+                    },
+                    modes:{
+                        repulse:{
+                            distance:20
+                        }
+                    }
+              },
+              repulse: {
+                distance: 100
+              },
+              push: {
+                particles_nb: 4
+              },}
+            }
+            } 
+              >
+              
+           
+            </Particles>
+            </div>
+      
+        <div id="base-container">
+        <Switch>
+
+<Route exact path="/cakes" >
+<MainTitle></MainTitle>
+<LeftDiv ></LeftDiv>
+<Items></Items>
+<Cakes></Cakes>
+</Route>
+
+<Route exact path='/orderCatalog/:id'>
+<OrderCatalog></OrderCatalog>
+<MainTitle></MainTitle>
+<LeftDiv ></LeftDiv>
+<Items></Items>
+<Cakes></Cakes>
+
+
+
+</Route>
+<Route exact path="/orderConfirmation" >
+<OrderConfirmation></OrderConfirmation>
+</Route>
+
+<Route exact path="/" >
+<MainTitle></MainTitle>
+<LeftDiv ></LeftDiv>
+<Items></Items>
+</Route>
+
+</Switch>
+</div>
+
+
       <div id="base-container">
            <Switch>
 
@@ -45,7 +157,8 @@ class App extends Component{
 
             </Switch>
       </div>
-    )
+      </div>
+    )       
   }
   
     
--- src/components/Cakes.js
+++ src/components/Cakes.js
@@ -2,28 +2,60 @@ import React,{Component} from 'react';
 
 import {Link} from 'react-router-dom'
 
+// import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+
 
 class Cakes extends Component{
 
+
+    //  ControlledCarousel() {
+    //     const [index, setIndex] = useState(0);
+      
+    //     const handleSelect = (selectedIndex, e) => {
+    //       setIndex(selectedIndex);
+    //     };
+    // }
+      
+    
+
     render()
     {
-        let user = new Array(91); for (let i=0; i<91; ++i) user[i] = 0;
+        let user = new Array(91); 
+        for (let i=0; i<91; ++i) user[i] = 0;
+
         
         return(
 
             <div id="cakes-container">
                   {  user.map((u,idx) =>
+
+                  
                      <div id="cakes">
-                    <img src={require('./cakes/'+(idx+1)+'.jpg') } alt='CakeImage'></img>
+                         {/* <AliceCarousel mouseTrackingEnabled> */}
+    
+    
+                    
+                      <img className="d-block w-100"
+                       src={require('./cakes/'+(idx+1)+'.jpg') } alt='CakeImage'
+      
+    />
+                   
                     <Link to={'/orderCatalog/'+idx} className="button" >Order</Link>
+                    <span aria-hidden="true" className="carousel-control-prev-icon" />
+                    {/* </AliceCarousel> */}
                     </div>
+                   
+
+                    
                   
                             
-                            )
+                  )   
                         }
-                
+                  
             </div>
         )
     }
+
 }
 export default Cakes;
